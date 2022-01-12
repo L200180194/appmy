45a650d0a583f0344cc81e40fbed2768650f9a15
L200180194 <alfiantoandy6@gmail.com>
2022-01-12 23:23:02 +0700

    Splash Screen, theme

---
+++ myintern/lib/pages/splash.dart
@@ -0,0 +1,33 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:myintern/theme.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({Key? key}) : super(key: key);
+
+  @override
+  _SplashPageState createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    Timer(Duration(seconds: 3), () => Navigator.pushNamed(context, '/signin'));
+    super.initState();
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: primaryColor,
+      body: Center(
+        child: Container(
+          width: 202,
+          height: 155,
+          decoration: BoxDecoration(
+              image: DecorationImage(image: AssetImage('assets/myintern.png'))),
+        ),
+      ),
+    );
+  }
+}
---
+++ myintern/lib/theme.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Color primaryColor = Color(0xff3F75C5);
+Color alertColor = Color(0xffEB1F2B);
+Color secondaryTextColor = Color(0xff82868B);
+Color primaryTextColor = Color(0xff050D18);
+Color bgGreen = Color(0xff04BB00);
+
+TextStyle primaryTextStyle = GoogleFonts.montserrat(color: primaryTextColor);
+TextStyle secondaryTextStyle =
+    GoogleFonts.montserrat(color: secondaryTextColor);
+FontWeight light = FontWeight.w300;
+FontWeight regular = FontWeight.w400;
+FontWeight medium = FontWeight.w500;
+FontWeight semibold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
